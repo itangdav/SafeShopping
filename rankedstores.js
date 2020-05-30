737474b886f12f30ac38840fd8cc567798e0bfb4
jerrry1123 <66176696+jerrry1123@users.noreply.github.com>
2020-05-30 15:44:36 -0400

    Add files via upload

---
+++ rankedstores.js
@@ -0,0 +1,14 @@
+import locations from './getlocation.js';
+console.log((locations));
+//starting css parts of the list.
+let liststring = '<nav>' + 
+'<ul>';
+
+for(let count = 0; count < locations.length; count++){
+    //this adds another item to the list.
+    liststring = liststring + '<li class = "firstHeading">' + locations[count][0] + 
+    ': Safety Score = ' + locations[count][3] + '</li>';
+}
+console.log(liststring);
+//sets the html item to be liststring.
+document.getElementById('list').innerHTML = liststring;
