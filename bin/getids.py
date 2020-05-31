70790bee5d78aa9bd71247d6ec30ec86db9bf3a2
David Tang <43974221+itangdav@users.noreply.github.com>
2020-05-31 15:20:20 -0400

    Added default location

--- bin/getids.py
+++ bin/getids.py
@@ -1,14 +1,21 @@
+import geocoder
 import googlemaps
 from bin.GetResultsMethod import API_KEY
 
 
 def getLocation(address):
-    gmaps = googlemaps.Client(key=API_KEY)
-    result = gmaps.geocode(address)
-    location = result[0]
-    lat =location['access_points'][0]['location']['latitude']
-    long =location['access_points'][0]['location']['longitude']
-    return [lat,long]
+    try:
+        gmaps = googlemaps.Client(key=API_KEY)
+        result = gmaps.geocode(address)
+        location = result[0]
+        lat =location['access_points'][0]['location']['latitude']
+        long =location['access_points'][0]['location']['longitude']
+        return [lat,long]
+    except:
+        g = geocoder.ip('me')
+        lat = g.latlng[0]
+        long = g.latlng[1]
+        return [lat, long]
 
 
 #print(getLocation("123 Front Street Toronto"))
